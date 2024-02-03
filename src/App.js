07d0d970a762a8658502e59866c0dee62f2607bc
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-02-03 19:10:21 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Nome from "./components/Nome";
+import {useState} from "react";
 
 const App = () => {
+
+    const [aluno, setAluno] = useState('Sujeito Programador')
+
+    const handleChangeName = (nome) =>{
+        setAluno(nome)
+    }
     return (
         <div>
             <h1>My frist app</h1>
+            <h2>Ola: {aluno}</h2>
+            <button onClick={() => handleChangeName("Bruno")}>
+                Mudar Nome
+            </button>
             <Nome name="Bruno" age={20}/>
-            <br/>
-            <Nome name="Silva" age={30}/>
         </div>
     )
 }
